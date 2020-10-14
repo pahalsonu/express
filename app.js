af1b968e1953c7fc5b53148607d5a4dbebf7634d
pahalsonu <pahalsonu@gmail.com>
2020-10-14 12:57:19 +0530

    "route chaining function working"

--- app.js
+++ app.js
@@ -2,22 +2,22 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+const path = require("path")
+// app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 
-app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+// app.use((req, res, next) =>{
+//     console.log(req.query);
+//     console.log("i am the primary hit")
+//     console.log(req.body)
+//     next();
+// });
 
-app.use((req, res, next) =>{
-    console.log(req.query);
-    console.log("i am the primary hit")
-    console.log(req.body)
-    next();
-});
+// app.use((req, res, next) =>{
+//     console.log(req.query);
+//     console.log("i am the secondry hit hit")
+//     next();
 
-app.use((req, res, next) =>{
-    console.log(req.query);
-    console.log("i am the secondry hit hit")
-    next();
-
-});
+// });
 
 
 
@@ -93,54 +93,103 @@ path*/
 // })
 
 /*home route learing middleware concept */
-app.get('/home', (req, res, next) => {
-    //query params
-    console.log(req.query);
-    console.log("i am hit 1");
-     req.some_value = "hello"
-    res.send('query params');
-    next();
+// app.get('/home', (req, res, next) => {
+//     //query params
+//     console.log(req.query);
+//     console.log("i am hit 1");
+//      req.some_value = "hello"
+//     res.send('query params');
+//     next();
 
-}, (req, res, next) => {
-    console.log(req.some_value);
-    console.log(req.query);
-    next();
+// }, (req, res, next) => {
+//     console.log(req.some_value);
+//     console.log(req.query);
+//     next();
 
-}
-    , (req, res) => {
-        console.log("i am hit 3");
-        console.log(req.query);
-    });
-//not working check again
-// const cb0 =    app.get('/home', (req, res, next) => {
-//         //query params
+// }
+//     , (req, res) => {
+//         console.log("i am hit 3");
 //         console.log(req.query);
-//         console.log("i am hit 1");
-//          req.some_value = "hello"
-//         res.send('query params');
-//         next();});
+//     });
+//not working check again
+// const cb0 = (req, res, next) => {
+//     //query params
+//     console.log(req.query);
+//     console.log("i am hit 1");
+//     req.some_value = "hello"
+//     res.send('query params');
+//     next();
+// };
 
-//         const cb1 =    app.get('/home', (req, res, next) => {
-//             //query params
-//             console.log(req.query);
-//             console.log("i am hit 2");
-//              req.some_value = "hello"
-//             res.send('query params');
-//             next();});
+// const cb1 = (req, res, next) => {
+//     //query params
+//     console.log(req.query);
+//     console.log("i am hit 2");
+//     req.some_value = "hello"
+//     res.send('query params');
+//     next();
+// };
 
-//             const cb2 =    app.get('/home', (req, res, next) => {
-//                 //query params
-//                 console.log(req.query);
-//                 console.log("i am hit 2");
-//                  req.some_value = "hello"
-//                 res.send('query params');
-//                 next();});
-//     app.get("/root", cb0, cb1, cb2) 
-    
+// const cb2 = (req, res, next) => {
+//     //query params
+//     console.log(req.query);
+//     console.log("i am hit 2");
+//     req.some_value = "hello"
+//     res.send('query params');
+// };
+// app.get("/root", cb0, cb1, cb2)
+//add two numbers
+// app.get('/add/:number1/:number2*', (req, res) => {
+//     //query params
+//     console.log(parseInt (req.params.number1)+parseInt((req.params.number2)))
+
+//     res.send()
+// });
 
 
+// app.get('/hello', (req, res) => {
+//     //query params
 
-    //next() example 2
+//     res.send("hello")
+// });
+
+// app.get('/:somepath', (req, res) => {
+//     //query params
+
+//     res.send(req.params)
+// });
+
+// const pdfpath = path.join(__dirname, 'd.pdf')
+// app.get('/download', (req, res) => {
+//     //query params
+//     console.log(pdfpath)
+//     res.download(pdfpath)
+// });
+
+// app.get('/redirect', (req, res) => {
+//     //query params
+//     console.log(pdfpath)
+//     res.redirect('/hello')
+// });
+
+
+// app.get('/hello', (req, res) => {
+//     //query params
+
+//     res.sendStatus(301)
+// });
+
+app.get('/hello', (req, res) => {
+    //query params
+
+    res.status(301).json({ "err": "moved" })
+});
+
+// http://localhost:3000/flights/Delhi-Amastardam
+// { '0': '', from: 'Delhi', to: 'Amastardam' }
+
+
+//next() example 2
 
 //why express over node.js
 //mvc model view controller
